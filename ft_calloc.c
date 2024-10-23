d39bd84f0a3c67c933dd890d9c21cdb9fb16321b
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-10-23 18:21:30 +0200

    auto update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tristan <tristan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 23:55:44 by tristan           #+#    #+#             */
-/*   Updated: 2024/10/23 18:18:27 by tristan          ###   ########.fr       */
+/*   Updated: 2024/10/23 18:21:25 by tristan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void    *ft_calloc(size_t count, size_t size)
     void    *res;
 
     if (count == SIZE_MAX || size == SIZE_MAX || ((long int)size < 0 && (long int)count < 0)
-        || ((int)count * (int)size < 0))
+        || count * size >= 2147483647)
         return (NULL);
     if (count == 0 || size == 0)
     {
