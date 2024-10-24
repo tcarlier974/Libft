4ef095fe404737e0b632efd8e692ba6d5c7b043e
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-10-24 18:23:49 +0200

    auto update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tristan <tristan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:09:42 by tristan           #+#    #+#             */
-/*   Updated: 2024/10/24 18:22:28 by tristan          ###   ########.fr       */
+/*   Updated: 2024/10/24 18:23:45 by tristan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	res = ft_strcat(res, s1);
 	res = ft_strcat(res, s2);
+	res[len] = '\0';
 	return (res);
 }
