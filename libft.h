41be6d9d0dd84961007d5b9bb9509c02e421e054
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-10-24 18:53:12 +0200

    auto update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tristan <tristan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:51:48 by tristan           #+#    #+#             */
-/*   Updated: 2024/10/23 13:12:29 by tristan          ###   ########.fr       */
+/*   Updated: 2024/10/24 18:52:58 by tristan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,5 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void    *ft_calloc(size_t count, size_t size);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 #endif
