7c0c979dd46f4c2af4aef428ff3ec262dcca5ed6
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-10-25 02:07:11 +0200

    auto update

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: tristan <tristan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:09:33 by tristan           #+#    #+#             */
-/*   Updated: 2024/10/22 22:52:48 by tristan          ###   ########.fr       */
+/*   Updated: 2024/10/25 02:07:06 by tristan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] && s2[i])
+	while ((char)s1[i] && (char)s2[i])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if ((char)s1[i] != (char)s2[i])
+			return ((char)s1[i] - (char)s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((char)s1[i] - (char)s2[i]);
 }
