2b19059db06137762b8c67e8a0b53465ebef7600
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-04 22:57:25 +0100

    auto update

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 20:35:06 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/04 22:56:58 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/04 22:57:22 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (*lst)
-		ft_lstlast(*lst)->next = new;
 	if (!lst || !new)
 		return ;
+	if (*lst)
+		ft_lstlast(*lst)->next = new;
 	else
 		*lst = new;
 }
