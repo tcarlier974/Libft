7ca6e900e32647ba20ad34c8d052b46b3ae27ef2
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-04 23:37:42 +0100

    auto update

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:11:34 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/04 23:37:11 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:37:40 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 		del((*lst)->content);
 		free(lst);
 	}
-	lst = NULL;
+	*lst = NULL;
 }
