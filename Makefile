b9bc1a3b8283eb035e9392b482976a286a4cf9c9
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-04 22:48:10 +0100

    auto update

--- Makefile
+++ Makefile
@@ -46,6 +46,7 @@ BONUS = ft_lstnew.c \
 			ft_lstadd_front.c \
 			ft_lstsize.c \
 			ft_lstlast.c \
+			ft_lstadd_back.c \
 
 OBJ = $(SRC:.c=.o)
 OBJ_BONUS = $(SRC:.c=.o) $(BONUS:.c=.o)
---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 20:35:06 by tcarlier          #+#    #+#             */
+/*   Updated: 2024/11/04 22:48:07 by tcarlier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	last = ft_lstlast(lst);
+	last->next = new;
+}
