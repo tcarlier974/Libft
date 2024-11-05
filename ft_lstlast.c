a3f656866621838bdf73020a32097628bfe9089c
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-05 15:37:44 +0100

    auto update

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tristan <tristan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:44:17 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/04 20:33:56 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:37:41 by tristan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ t_list	*ft_lstlast(t_list *lst)
 	if (!lst)
 		return (lst);
 	while (lst->next)
-	{
 		lst = lst->next;
-	}
 	return (lst);
 }
