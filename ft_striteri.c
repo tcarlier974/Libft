dbdbfcdf0de155dfc5e820b2a3c599b5e37c290c
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-06 14:18:00 +0100

    auto update

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:16:03 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/06 14:17:18 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:17:58 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int,char*))
 	i = 0;
 	while (s[i])
 	{
-		s[i] = f(i, s);
+		f(i, s);
 		i++;
 	}
 }
