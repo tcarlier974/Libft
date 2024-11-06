357690051c746389f961c9017d9d8db2c3098360
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-06 14:32:56 +0100

    auto update

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:02:57 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/04 23:10:11 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:32:53 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
+	if (!lst || !del)
+		return ;
 	del(lst->content);
 	free(lst);
 }
